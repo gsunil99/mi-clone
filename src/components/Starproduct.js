f7c11b2949d275259df4d6a10c53360c07e20794
Sunil G <gsunil99910@gmail.com>
2023-05-25 21:39:24 +0530

    added space

--- src/components/Starproduct.js
+++ src/components/Starproduct.js
@@ -5,15 +5,15 @@ const Starproduct = ({ starProduct }) => {
   return (
     <div className="starProduct">
       <div>
-        <a href={starProduct[0].url}></a>
+        <a href={starProduct[0].url}> </a>
         <img src={starProduct[0].image} alt="product image1"></img>
       </div>
       <div>
-        <a href={starProduct[1].url}></a>
+        <a href={starProduct[1].url}> </a>
         <img src={starProduct[1].image} alt="product image2"></img>
-        <a href={starProduct[2].url}></a>
+        <a href={starProduct[2].url}> </a>
         <img src={starProduct[2].image} alt="product image3"></img>
-        <a href={starProduct[3].url}></a>
+        <a href={starProduct[3].url}> </a>
         <img src={starProduct[3].image} alt="product image4"></img>
       </div>
     </div>
